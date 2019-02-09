835363237a1948293a97788af2a730b4344aaeee
admin <admin@iMac-de-admin-4.local>
2019-02-10 00:30:05 +0100

    Filter on choice_screen

---
+++ assets/js/choose_screen.js
@@ -0,0 +1,31 @@
+ var elems = document.getElementsByClassName('token');
+
+ var filter = ['jobs', 'help', 'hobby', 'project'];
+
+ var apply_filter = () => {
+     for (let i = 0; i < elems.length; i++) {
+         let classes = elems[i].classList;
+         for (let j = 0; j < classes.length; j++) {
+             let display = false;
+             filter.forEach((v) => {
+                 console.log('v: ', v);
+                 if (classes[j] == v) {
+                     display = true;
+                     return ;
+                 }
+             });
+             elems[i].style.display = display ? 'block' : 'none';
+         }
+     }
+ }
+
+ var f_select = (elem) => {
+     console.log('elem: ', elem);
+     if (filter.includes(elem)) {
+         filter.splice(filter.indexOf(elem), 1);
+     } else {
+         filter.push(elem);
+     }
+     apply_filter();
+     console.log('filter: ', filter);
+ }
