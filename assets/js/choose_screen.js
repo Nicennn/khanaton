b0c275329dbdfb0637f95b87c87e54de838c749a
admin <admin@iMac-de-admin-4.local>
2019-02-10 11:59:57 +0100

    wqefqewfqwef

--- assets/js/choose_screen.js
+++ assets/js/choose_screen.js
@@ -1,3 +1,34 @@
+var elems = document.getElementsByClassName('token');
+var filter = ['jobs', 'help', 'hobby', 'project'];
+
+var apply_filter = () => {
+    for (let i = 0; i < elems.length; i++) {
+        let classes = elems[i].classList;
+        for (let j = 0; j < classes.length; j++) {
+            let display = false;
+            filter.forEach((v) => {
+                console.log('v: ', v);
+                if (classes[j] == v) {
+                    display = true;
+                    return ;
+                }
+            });
+            elems[i].style.display = display ? 'block' : 'none';
+        }
+    }
+}
+
+var f_select = (elem) => {
+    console.log('elem: ', elem);
+    if (filter.includes(elem)) {
+        filter.splice(filter.indexOf(elem), 1);
+    } else {
+        filter.push(elem);
+    }
+    apply_filter();
+    console.log('filter: ', filter);
+}
+
 window.onload = () => {
     var GLOBAL = JSON.parse(localStorage.getItem('GLOBAL'));
     console.log('GOLBAL: ', GLOBAL);
@@ -6,37 +37,4 @@ window.onload = () => {
     if (name) {
         name.innerHTML = "Bonjour " + GLOBAL.name + '!';
     }
-
-    var elems = document.getElementsByClassName('token');
-
-    var filter = ['jobs', 'help', 'hobby', 'project'];
-
-    var apply_filter = () => {
-        for (let i = 0; i < elems.length; i++) {
-            let classes = elems[i].classList;
-            for (let j = 0; j < classes.length; j++) {
-                let display = false;
-                filter.forEach((v) => {
-                    console.log('v: ', v);
-                    if (classes[j] == v) {
-                        display = true;
-                        return ;
-                    }
-                });
-                elems[i].style.display = display ? 'block' : 'none';
-            }
-        }
-    }
-
-    var f_select = (elem) => {
-        console.log('elem: ', elem);
-        if (filter.includes(elem)) {
-            filter.splice(filter.indexOf(elem), 1);
-        } else {
-            filter.push(elem);
-        }
-        apply_filter();
-        console.log('filter: ', filter);
-    }
-
 }
